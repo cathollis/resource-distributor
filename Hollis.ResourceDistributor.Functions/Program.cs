97ddb7a1471a729aaf7e2f5af8b4fc6898b2f072
hollis <243184473+cathollis@users.noreply.github.com>
2026-02-01 00:19:41 +0800

    feat: add application insight

--- Hollis.ResourceDistributor.Functions/Program.cs
+++ Hollis.ResourceDistributor.Functions/Program.cs
@@ -2,6 +2,7 @@ using Hollis.ResourceDistributor.Functions;
 using Hollis.ResourceDistributor.Functions.Configs;
 using Hollis.ResourceDistributor.Ip2LocationClient.Configs;
 using Hollis.ResourceDistributor.Ip2LocationClient.Extensions;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -10,14 +11,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
 var projectName = nameof(ResourceDistributorDbContext).Replace(nameof(DbContext), string.Empty);
+
 var builder = FunctionsApplication.CreateBuilder(args);
-
 builder.ConfigureFunctionsWebApplication();
-
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
 
 builder.Services.Configure<AppConfig>(builder.Configuration.GetSection(nameof(AppConfig)));
 
@@ -31,5 +27,8 @@ builder.Services.AddDbContext<ResourceDistributorDbContext>(options =>
         sqlServer.MigrationsHistoryTable("__EFMigrationsHistory", projectName);
     });
 });
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
 
 builder.Build().Run();
